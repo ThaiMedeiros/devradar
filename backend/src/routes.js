8ac7a2dedee50bbde84e9f554e1a75034db30575
Thaiza Medeiros <thaizamedeirossw@gmail.com>
2020-05-30 02:21:59 -0300

    Configurações de plugins.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,12 @@
-const { Router } = require('express');
-const DevController = require('./controllers/DevController');
-const SearchController = require('./controllers/SearchController');
+const { Router } = require("express");
+const DevController = require("./controllers/DevController");
+const SearchController = require("./controllers/SearchController");
 
 const routes = Router();
 
-routes.get('/devs', DevController.index); //listar devs
-routes.post('/devs', DevController.store); //cadastrar devs
+routes.get("/devs", DevController.index); //listar devs
+routes.post("/devs", DevController.store); //cadastrar devs
 
-routes.get('/search', SearchController.index); //buscar dev passando parâmetros
+routes.get("/search", SearchController.index); //buscar dev passando parâmetros
 
 module.exports = routes;
