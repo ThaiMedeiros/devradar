988142cb2ad9bf1d83e798ba53dc71f73a5bbd36
Thaiza Medeiros <thaizamedeirossw@gmail.com>
2020-05-30 22:43:45 -0300

    Configurações no backend da aplicação.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,17 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const routes = require('./routes');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const routes = require("./routes");
 
 const app = express();
 
-mongoose.connect('mongodb+srv://developer:developer@cluster0-pafje.mongodb.net/devradar?retryWrites=true&w=majority', {
+mongoose.connect(
+  "mongodb+srv://developer:developer@cluster0-pafje.mongodb.net/devradar?retryWrites=true&w=majority",
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-});
+  }
+);
 
 app.use(express.json());
 
@@ -17,8 +21,9 @@ app.use(express.json());
 /*
  * Query params: request.query (filtros, ordenação, paginação, etc..)
  * Route Params: request.params (identificar recurso na alteração ou remoção / o nomde do recurso aparece na url após '?')
- * Body: request.body (dados para criação ou alteração de um registro / o nomde do recurso é indicado na criação da rota)  
-*/
+ * Body: request.body (dados para criação ou alteração de um registro / o nomde do recurso é indicado na criação da rota)
+ */
 
+app.use(cors());
 app.use(routes);
 app.listen(3333);
